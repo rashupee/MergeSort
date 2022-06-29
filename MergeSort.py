00cdbabc16ab7b5cfe517b456c12a1f7afc33815
rashupee <rashupee@gmail.com>
2022-06-29 07:45:40 -0700

    Initial MergeSort project commit

---
+++ MergeSort.py
@@ -0,0 +1,17 @@
+
+def merge_sort(array_1, array_2):
+	result = []
+	for element_1 in array_1:
+		for element_2 in array_2:
+			val = min(element_1, element_2)
+			result.append(val)
+
+	return result
+
+
+
+def merge_sorted(array):
+	answer = []
+
+
+	return answer
