47535cd2c188be3a48ee1377a6f05d5d5bd46c63
rashupee <rashupee@gmail.com>
2022-06-30 08:05:09 -0700

    merge sort of two sorted lists working

--- MergeSort.py
+++ MergeSort.py
@@ -1,17 +1,38 @@
 
-def merge_sort(array_1, array_2):
-	result = []
-	for element_1 in array_1:
-		for element_2 in array_2:
-			val = min(element_1, element_2)
-			result.append(val)
 
+
+
+
+
+def merge_sort(a1, a2):
+	result = []
+
+	a1_finished, a2_finished = not bool(a1), not bool(a2)
+	i, j = 0, 0
+	while not a1_finished and not a2_finished:
+		if a1[i] <= a2[j]:
+			result.append(a1[i])
+			i += 1
+			if i == len(a1):
+				a1_finished = True
+		else:
+			result.append(a2[j])
+			j += 1
+			if j == len(a2):
+				a2_finished = True
+	if not a1_finished:
+		for index in range(i, len(a1)):
+			result.append(a1[index])
+	if not a2_finished:
+		for index in range(j, len(a2)):
+			result.append(a2[index])
+	
 	return result
+
 
 
 
 def merge_sorted(array):
 	answer = []
-
 
 	return answer
